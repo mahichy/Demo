c18bca9d09358915c19b3ae9851c7d15942bd018
mahi <mohiddinoshim@yahoo.com>
2022-06-10 11:20:41 +0600

    image problem done

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,7 @@ class Project < ApplicationRecord
 	has_rich_text :body
 
 	def image_as_thumbnail
-		return unless image.content_type.in?(%w[iamge/jpeg image/png])
+		return unless image.content_type.in?(%w[image/jpeg image/png])
 		image.variant(resize_to_limit: [300, 300]).processed
 	end
 
